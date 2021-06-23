158edd175fc7d13ec8a0ca4357776570d77cbc63
AntonioSkopin <askopin@outlook.com>
2021-06-23 22:33:17 +0200

    #9 Setup frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-gray-100 w-screen h-screen">
+      <h1 className="text-6xl font-bold text-gray-800">Newpapr.</h1>
+    </div>
+  );
+}
